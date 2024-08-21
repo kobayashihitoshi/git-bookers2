9ef8937e009adadf79dca2ebe3d9b54d9fa19081
EC2 Default User <ec2-user@ip-172-31-17-137.ap-northeast-1.compute.internal>
2024-08-21 09:28:17 +0000

    Install Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+  end
+  
+  def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def edit
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
+      @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+end
